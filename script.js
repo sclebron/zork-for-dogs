8270a2deb08869fcb74aef0cbd416372ed1c3656
SLeBron <sophieclebron@gmail.com>
2023-01-18 13:03:30 -0700

    added rooms object

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+var rooms = {
+    "room0": "<p>You are on a snow covered field. You see a hill up ahead and you see a group of deer to your right.</p>"
+}
+
 $(document).ready(function(){
     $(document).keypress(function(key){
         //if statement that checks if user presses Enter and is in user-input box
