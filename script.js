d1abcd3bc42ffc16b250db7132ada235fc3a078e
SLeBron <sophieclebron@gmail.com>
2023-01-18 12:44:06 -0700

    added if statement to see when user presses Enter in user-input box

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
 var input = $('#user-input').val();
 
 $(document).ready(function(){
-    $(document).keypress(function(){
-        alert("hello");
+    $(document).keypress(function(key){
+        //if statement that checks if user presses Enter and is in user-input box
+        if (key.which === 13 && $('#user-input').is(':focus')) {
+            alert("hello");
+        }
     })
 })
