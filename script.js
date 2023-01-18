73c6fe11f8c20e7e640116d8c80a7e5eea0516ff
SLeBron <sophieclebron@gmail.com>
2023-01-18 13:05:31 -0700

    added append method

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ var rooms = {
 }
 
 $(document).ready(function(){
+    $('#game-text').append()
     $(document).keypress(function(key){
         //if statement that checks if user presses Enter and is in user-input box
         if (key.which === 13 && $('#user-input').is(':focus')) {
