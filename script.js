0d2b39ac2f7b28973eee315b02f0a93a9fe8b593
SLeBron <sophieclebron@gmail.com>
2023-01-19 09:57:42 -0700

    added rooms

--- script.js
+++ script.js
@@ -2,7 +2,12 @@ var rooms = {
     "start": {
         "description": "You are on a snow covered field. You see a hill up ahead and you see a group of deer to your right."
     },
-
+    "hill": {
+        "description": "You run to the top of the hill. You can roll around in the snow at the top of the hill or slide down the hill."
+    },
+    "deer": {
+        "description": "You run over to the group of deer. One of the deer walks up to you. You can walk up to the deer or run past the deer to a cabin behind the deer."
+    }
 }
 
 var currentRoom = "start";
