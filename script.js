fe1e48bdcb4d1db1309852520e5a62c46aec50b0
SLeBron <sophieclebron@gmail.com>
2023-01-19 09:34:53 -0700

    changed format of accessing rooms

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ var rooms = {
 
 
 $(document).ready(function(){
-    $('#game-text').append(rooms["room0"])
+    $('#game-text').append(rooms.start.description)
     $(document).keypress(function(key){
         //if statement that checks if user presses Enter and is in user-input box
         if (key.which === 13 && $('#user-input').is(':focus')) {
