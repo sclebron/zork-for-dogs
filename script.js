54792e43b341d05c5f8dd76c569534b431a1b661
SLeBron <sophieclebron@gmail.com>
2023-01-23 12:38:19 -0700

    case insensitive

--- script.js
+++ script.js
@@ -20,7 +20,7 @@ $(document).ready(function(){
     $(document).keypress(function(key){
         //if statement that checks if user presses Enter and is in user-input box
         if (key.which === 13 && $('#user-input').is(':focus')) {
-            var value = $('#user-input').val();
+            var value = $('#user-input').val().toLowerCase();
              switch(value) {
                 case "hill":
                     alert("You have gone to the hill");
