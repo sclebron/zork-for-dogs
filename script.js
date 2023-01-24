072c4133401940884cb13b9ddda4d61a84ec5a9a
SLeBron <sophieclebron@gmail.com>
2023-01-24 09:55:35 -0700

    added rooms.js file

--- script.js
+++ script.js
@@ -1,25 +1,3 @@
-var rooms = {
-    "start": {
-        "description": "You are on a snow covered field. You see a hill up ahead and you see a group of deer to your right.",
-        "directions": {
-            "hill": "hill",
-            "deer": "deer"
-        }
-    },
-    "hill": {
-        "description": "You run to the top of the hill. You can roll around in the snow at the top of the hill or slide down the hill.",
-        "directions": {}
-    },
-    "deer": {
-        "description": "You run over to the group of deer. One of the deer walks up to you. You can walk up to the deer or run past the deer to a cabin behind the deer.",
-        "directions": {}
-    },
-    "roll": {
-        "description": "You roll around in the snow at the top of the hill. So fun! You eat some snow and rub your face in it and then jump up. From the top of the hill you see a large group of animals running in a circle in the distance. To the left of the animals you see a frozen lake.",
-        "directions": {}
-    },
-}
-
 var currentRoom = "start";
 
 $(document).ready(function(){
