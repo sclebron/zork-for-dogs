3918c78f473e359be9cd263bdf06cfe2a3961212
SLeBron <sophieclebron@gmail.com>
2023-01-31 13:04:38 -0700

    replace switch statement with function

--- script.js
+++ script.js
@@ -9,28 +9,17 @@ function changeRoom(direction) {
     }
 }
 
+function playerInput() {
+    
+}
+
 $(document).ready(function(){
     $('#game-text').append("<p>" + rooms.start.description + "</p>")
     $(document).keypress(function(key){
         //if statement that checks if user presses Enter and is in user-input box
         if (key.which === 13 && $('#user-input').is(':focus')) {
             var value = $('#user-input').val().toLowerCase();
-            switch(value) {
-                case "hill":
-                    changeRoom("hill");
-                    break;
-                case "deer":
-                    changeRoom("deer");
-                    break
-                case "roll":
-                    changeRoom("roll");
-                    break
-                case "slide": 
-                    changeRoom("slide");
-                    break
-                default: 
-                    alert("Invalid move");
-            }
+            playerInput(value)
         }
     })
 })
