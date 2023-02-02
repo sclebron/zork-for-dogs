e91dbcb3cbb7dd775f4f132f4d0c0647ad8a5293
SLeBron <sophieclebron@gmail.com>
2023-02-02 11:44:45 -0700

    clear text after enter

--- script.js
+++ script.js
@@ -57,6 +57,7 @@ $(document).ready(function(){
         //if statement that checks if user presses Enter and is in user-input box
         if (key.which === 13 && $('#user-input').is(':focus')) {
             var value = $('#user-input').val().toLowerCase();
+            $('#user-input').val('');
             playerInput(value)
         }
     })
