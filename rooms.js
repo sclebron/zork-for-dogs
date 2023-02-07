767bb38ce19382f090148421060b81b0eb55bcd7
SLeBron <sophieclebron@gmail.com>
2023-02-07 14:24:19 -0700

    deleted original room data

--- rooms.js
+++ rooms.js
@@ -5,26 +5,31 @@ var rooms = {
             "deer": "deer",
             "shrub": "shrub"
         },
-        "items": {
-            "sword": "You see a shimmering light in a bush. "
-        },
-        "npcs": {
-            "old lady": "The old lady emits a strange noise and attacks you."
-        }
-    },
-    "hill": {
-        "description": "You run to the top of the hill. You can roll around in the snow at the top of the hill or slide down the hill.",
-        "directions": {
-            "roll": "roll",
-            "slide": "slide"
-        }
     },
     "deer": {
-        "description": "You run over to the group of deer. One of the deer walks up to you. You can walk up to the deer or run past the deer to a cabin behind the deer.",
-        "directions": {}
+        "description": "",
+        "directions": {
+            "": "",
+            "": ""
+        }
     },
-    "roll": {
-        "description": "You roll around in the snow at the top of the hill. So fun! You eat some snow and rub your face in it and then jump up. From the top of the hill you see a large group of animals running in a circle in the distance. To the left of the animals you see a frozen lake.",
-        "directions": {}
+    "shrub": {
+        "description": "",
+        "directions": {
+
+        }
+    },
+    "": {
+        "description": "",
+        "directions": {
+
+        }
     },
 }
+
+// "items": {
+//     "sword": "You see a shimmering light in a bush. "
+// },
+// "npcs": {
+//     "old lady": "The old lady emits a strange noise and attacks you."
+// }        
