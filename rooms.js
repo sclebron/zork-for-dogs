85b1d61dde34b20b57d7c566d0420687801ce65c
SLeBron <sophieclebron@gmail.com>
2023-02-07 13:43:07 -0700

    updated rooms

--- rooms.js
+++ rooms.js
@@ -2,8 +2,8 @@ var rooms = {
     "start": {
         "description": "You are on a snow covered field. To your right you see a group of deer and to your left you see a shrub. You can go right to the deer or left to the shrub.",
         "directions": {
-            "hill": "hill",
-            "deer": "deer"
+            "deer": "deer",
+            "shrub": "shrub"
         },
         "items": {
             "sword": "You see a shimmering light in a bush. "
