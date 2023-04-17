6495a421cf95303e1fcb4655a2497500c575ddd8
SLeBron <sophieclebron@gmail.com>
2023-04-17 15:20:04 -0600

    changing definition of direction

--- script.js
+++ script.js
@@ -4,49 +4,45 @@ var help = "";
 
 const direction1 = rooms[currentRoom].directions.direction1;
 const direction2 = rooms[currentRoom].directions.direction2;
-let direction = 
+// if (user-input.includes(direction1) == true) {
+//     let direction = direction1;
+// } else if (user-input.includes(direction2) == true) {
+//     let direction = direction2;
+// }
 
 function changeRoom(direction) {
+    // if (user-input.includes(direction1) == true) {
+    //     let direction = direction1;
+    // } else if (user-input.includes(direction2) == true) {
+    //     let direction = direction2;
+    // }
     if(rooms[currentRoom].directions[direction] !== undefined) {
         currentRoom = rooms[currentRoom].directions[direction];
         //need to define direction or change how this function works, right now direction is undefined
+        console.log(direction)
         $('#game-text').append('<p>' + rooms[currentRoom].description + '</p>');
     } else {
         $('#game-text').append('<p>You cannot go that way!</p>');
+        console.log(direction)
     }
 }
-
-
 
 function showHelp() {
     $('#game-text').append('<p>Here is how to play: </p>');
     $('#game-text').append(help);
 }
 
-// function playerInput(input) {
-//     var command = input.split(' ')[0];
-//     switch (command) {
-//         case "go":
-//             // var direction = input.split(' ')[1];
-//             var direction = rooms.directions;
-//             changeRoom(direction);
-//             break;
-//         case "help":
-//             showHelp();
-//             break;
-//         default: 
-//             $('#game-text').append('<p>Invalid command</p>');
-//     }
-// }
-
-/*change switch statement to if statement because of command  */
-
 function playerInput(input) {
+    // if (input.includes(direction1) == true) {
+    //     let direction = direction1;
+    // } else if (input.includes(direction2) == true) {
+    //     let direction = direction2;
+    // }
+    // let direction = rooms.directions;
     if (input === "help") {
         showHelp();
     } else if (input.includes(direction1) == true || input.includes(direction2) == true) {
         changeRoom();
-        //changeRoom is undefined error
     } else if (input.includes(direction1) == false && input.includes(direction2) == false) {
         $('#game-text').append('<p>Invalid command</p>');
     }
@@ -76,3 +72,20 @@ $(document).ready(function(){
 //         }
 //     $('#game-text').append('</ul></p>');
 // }
+
+// function playerInput(input) {
+//     var command = input.split(' ')[0];
+//     switch (command) {
+//         case "go":
+//             // var direction = input.split(' ')[1];
+//             var direction = rooms.directions;
+//             changeRoom(direction);
+//             break;
+//         case "help":
+//             showHelp();
+//             break;
+//         default: 
+//             $('#game-text').append('<p>Invalid command</p>');
+//     }
+// }
+
