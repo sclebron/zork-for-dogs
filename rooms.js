306d7e739d4b51cc173e7bab3a45fb3cb598f46f
SLeBron <sophieclebron@gmail.com>
2023-04-18 15:59:53 -0600

    updated rooms.js

--- rooms.js
+++ rooms.js
@@ -10,28 +10,28 @@ var rooms = {
         "description": "You walk up to the deer. You jump around and play with a few of the young deer. The deer appear to hear something to the left and get startled. They run to the right. You can follow the deer to the right or go left towards the noise.",
         "directions": {
             "direction1": "follow-deer",
-            "noise": "noise"
+            "direction2": "noise"
         }
     },
     "shrub": {
         "description": "As you approach the shrub you catch the scent of some people nearby. You can follow the scent or look behind the shrub.",
         "directions": {
             "direction1": "scent",
-            "behind-shrub": "behind-shrub"
+            "direction2": "behind-shrub"
         }
     },
     "follow-deer": {
         "description": "You follow the deer up a hill. At the top of the hill you see a plume of smoke to the left and a frozen lake to the right. You can go left towards the smoke or right to the lake.",
         "directions": {
             "direction1": "smoke",
-            "lake": "lake"
+            "direction2": "lake"
         }
     },
     "noise": {
         "description": "You go towards the noise, which leads you into a forest. Through the trees you see a moose. You can walk up to the moose or turn and run away.",
         "directions": {
             "direction1": "moose",
-            "run": "run"
+            "direction2": "run"
         }
     },
     "scent": {
@@ -45,42 +45,42 @@ var rooms = {
         "description": "You see a hole in the snow and as well as a pile of berries. You can dig in the hole or eat the berries.",
         "directions": {
             "direction1": "hole",
-            "berries": "berries"
+            "direction2": "berries"
         }
     },
     "smoke": {
         "description": "You run to the smoke. You see a man, huskies, and a sled around a fire. You can approach the man or the huskies.",
         "directions": {
             "direction1": "man",
-            "huskies": "huskies"
+            "direction2": "huskies"
         }
     },
     "lake": {
         "description": "",
         "directions": {
             "direction1": "fhdiao",
-            "right": ""
+            "direction2": ""
         }
     },
     "moose": {
         "description": "",
         "directions": {
             "direction1": "fhdia",
-            "": ""
+            "direction2": ""
         }
     },
     "run": {
         "description": "",
         "directions": {
             "direction1": "fd",
-            "": ""
+            "direction2": ""
         }
     },
     "man": {
         "description": "",
         "directions": {
             "direction1": "efr",
-            "": ""
+            "direction2": ""
         }
     },
     "huskies": {
@@ -94,21 +94,21 @@ var rooms = {
         "description": "",
         "directions": {
             "direction1": "q2",
-            "": ""
+            "direction2": ""
         }
     },
     "berries": {
         "description": "",
         "directions": {
             "direction1": "few",
-            "": ""
+            "direction2": ""
         }
     },
     "sleep": {
         "description": "",
         "directions": {
             "direction1": "",
-            "": ""
+            "direction2": ""
         }
     },
 }
